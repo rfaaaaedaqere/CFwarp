6a6908b7562452e5e40bcf1c195542c6a05cd7a3
kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-01-06 03:56:19 +0800

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -56,8 +56,8 @@ c1="sed -i '/0\.0\.0\.0\/0/d' wgcf-profile.conf"
 c2="sed -i '/\:\:\/0/d' wgcf-profile.conf"
 c3="sed -i 's/engage.cloudflareclient.com/162.159.192.1/g' wgcf-profile.conf"
 c4="sed -i 's/engage.cloudflareclient.com/2606:4700:d0::a29f:c001/g' wgcf-profile.conf"
-c5="sed -i 's/1.1.1.1/1.1.1.1,8.8.8.8,2606:4700:4700::1111,2001:4860:4860::8888/g' wgcf-profile.conf"
-c6="sed -i 's/1.1.1.1/2606:4700:4700::1111,2001:4860:4860::8888,1.1.1.1,8.8.8.8/g' wgcf-profile.conf"
+c5="sed -i 's/1.1.1.1/8.8.8.8,2001:4860:4860::8888/g' wgcf-profile.conf"
+c6="sed -i 's/1.1.1.1/2001:4860:4860::8888,8.8.8.8/g' wgcf-profile.conf"
 yellow " 请稍等3秒……正在扫描vps类型及参数中……"
 
 ShowWGCF(){
