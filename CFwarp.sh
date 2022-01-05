0d660289350f4ebd11efabd44cf305791db1ba64
kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-01-06 05:07:14 +0800

    Update CFwarp.sh

--- CFwarp.sh
+++ CFwarp.sh
@@ -43,7 +43,7 @@ uname -m | grep -q -E -i "aarch" && cpu=ARM64 || cpu=AMD64
 vi=`systemd-detect-virt`
 if [[ -n $(sysctl net.ipv4.tcp_congestion_control | awk -F ' ' '{print $3}') ]]; then
 bbr=`sysctl net.ipv4.tcp_congestion_control | awk -F ' ' '{print $3}'`
-elif [[ systemctl is-active lkl-haproxy = active ]]; then
+elif [[ $(systemctl is-active lkl-haproxy) = active ]]; then
 bbr="openvz版BBR-PLUS"
 else
 bbr="暂不支持显示"
